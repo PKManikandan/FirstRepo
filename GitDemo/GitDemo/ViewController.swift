34e36e9baccf64531353f50035b830d49ef9c77b
Manikandan Krishnamoorthi <manikandan.krishnamoorthi@impigertech.com>
2016-10-20 13:27:03 +0530

    Initial Commit

---
+++ GitDemo/GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Manikandan on 20/10/16.
+//  Copyright © 2016 Impiger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World!")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
+
